46af9d5b294f7b46a2ea263691c7f5c4e3472a15
Zhenya <EvgeniyWide@gmail.com>
2016-04-12 22:30:58 +0300

    authentication WIP

---
+++ server/router.js
@@ -0,0 +1,14 @@
+const Authentication = require('./controllers/authentication');
+
+
+module.exports = function (app) {
+
+	app.post('/signup', Authentication.signup);
+	
+	// app.get('/', function(req, res, next){
+	// 	res.send(['phone', 'paper']);
+	// });
+
+
+
+}
