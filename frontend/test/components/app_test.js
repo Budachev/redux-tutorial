85ba7364ad1fc08f0266765f2470c797c0895ca5
ybudachov <ybu@ciklum.com>
2016-05-19 18:09:47 +0300

    first test for CommentBox component

--- frontend/test/components/app_test.js
+++ frontend/test/components/app_test.js
@@ -2,12 +2,13 @@ import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
 describe('App' , () => {
-  let component;  
 
-  it('renders something', () => {
+  it('Show correct text', () => {
+
+    //create component
   	const component = renderComponent(App);
 
     expect(component).to.contain('React simple starter');
-  });
-  
+  })
+
 });
