f04ffe88a96ab54f36c4294c8b5b24dc99fec50f
ybudachov <ybu@ciklum.com>
2016-05-25 11:21:03 +0300

    add actions and reducers both tests and code

---
+++ frontend/test/actions/actions_index_test.js
@@ -0,0 +1,18 @@
+import { expect } from '../test_helper';
+import { SAVE_COMMENT } from '../../src/actions/types';
+import { saveComment } from '../../src/actions';
+
+describe('actions', () => {
+	describe('saveComment', () => {
+		it('Has the correct type', () => {
+			const action = saveComment();
+			expect(action.type).to.equal(SAVE_COMMENT);
+		});
+
+		it('Has the correct payload', () => {
+			const action = saveComment('new commenet');
+			expect(action.payload).to.equal('new commenet');
+		});
+
+	});
+});
